0aa246072af8a97fe36d28806bd94599938945ae
Isabellak16 <79188776+Isabellak16@users.noreply.github.com>
2021-05-14 17:00:24 +0930

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var gameState = 0
+var playerCount
+var database
+var player,game,form
+
+function setup(){
+ canvas = createCanvas(400,400) 
+ database = firebase.database()
+ game = new Game()
+ game.getState()
+ game.start()
+
+}
+
+function draw(){
+  
+}
